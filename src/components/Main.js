f318811b101452224baadb81c10ac178ec1d10c7
anahibravo <anahi.bravo@protonmail.com>
2024-04-07 22:57:39 -0700

    Main landing page

---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+
+export default function Main(){
+    return(
+    <div className="main--container">
+        <h1 className="main--heading">
+            Monthly Statistics
+        </h1>
+        <button className="main--button"> Start </button>
+    </div>
+    )
+}
