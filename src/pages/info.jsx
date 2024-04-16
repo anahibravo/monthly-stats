c73750ed9538cf74ce38fa195529ca5305a1bc88
anahibravo <anahi.bravo@protonmail.com>
2024-04-16 10:13:34 -0700

    added styling to the form

--- src/pages/info.jsx
+++ src/pages/info.jsx
@@ -3,8 +3,31 @@ import React from "react";
 
 export function Info(){
     return (
-        <div>
-            <h1>this is the information page</h1>
-        </div>
+    <div className="info--container">
+        <h1 className="info--heading">Information</h1>
+
+    <form className="info--form">   
+        <label className="info--input">
+        Library Name <input name="libraryname" />
+        </label>
+
+        <label className="info--input">
+        Month <input name="month" />
+        </label>
+
+        <label className="info--input">
+        Year <input name="year" />
+        </label>
+
+        <label className="info--input">
+        Upload <input type="file"/>
+        </label>
+
+        <button className="info--button" type="submit">GO</button>
+
+    </form> 
+
+
+    </div>
     )
 }
